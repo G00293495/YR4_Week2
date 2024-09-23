7035bfeaf90a5150f880e8eaac0c0f2f44cad3e5
Paddy Downey <G00392495@atu.ie>
2024-09-23 16:03:45 +0100

    New project created

---
+++ src/main/java/ie/atu/week2/week2_refresher/Week2Refresher.java
@@ -0,0 +1,15 @@
+package ie.atu.week2.week2_refresher;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Week2Refresher {
+
+    @GetMapping("/hello")
+    public String message()
+    {
+        return "Hello";
+    }
+}
