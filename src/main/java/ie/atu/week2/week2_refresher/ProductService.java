2921cfdc55db0327e2d5a3f9564a3cc2bd2bae71
G00392495@atu.ie <G00392495@atu.ie>
2024-09-24 11:55:22 +0100

    added dependancy injection

--- src/main/java/ie/atu/week2/week2_refresher/ProductService.java
+++ src/main/java/ie/atu/week2/week2_refresher/ProductService.java
@@ -1,4 +1,20 @@
 package ie.atu.week2.week2_refresher;
 
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class ProductService {
+    private List<Product> productlist = new ArrayList<Product>();
+
+    public List<Product> getAllProducts() {
+        return productlist;
+    }
+    public Product addProduct(Product product) {
+        productlist.add(product);
+        return product;
+    }
 }
